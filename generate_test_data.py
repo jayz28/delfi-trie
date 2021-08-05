86c96dd00f7370b2f104a1a6d4e3f91bb2d24ec7
Jay <jay@resolutionbio.com>
2021-08-04 22:15:48 -0700

    initial commit

---
+++ generate_test_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import random
+
+
+def main(args):
+    read_length: int = int(args[0])
+    num_reads: int = int(args[1])
+
+    bases: [str] = ["A", "T", "C", "G", "N"]
+
+    for _ in range(num_reads):
+        read: str = "".join(random.choices(bases, k=read_length))
+        print(read)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
