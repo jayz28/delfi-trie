46fe36f717fad226602435674965a14d382ff81a
Jay Lee <jay@resolutionbio.com>
2021-08-06 15:44:26 -0700

    reordered find

--- TreeNode.py
+++ TreeNode.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
-from typing import MutableMapping, NewType, Union, NamedTuple, Optional
+from typing import MutableMapping, NewType, Union, NamedTuple, Optional, cast
 from collections import namedtuple
+import sys
 
 
-# define some type protocols
+# define some type protocols to improve readability and leverge type checking
+# using NamedTuple here for a lighterwight native implementation vs @dataclass
 class SplitResult(NamedTuple):
     matched_prefix: str = ""
     remaining_prefix: str = ""
-    remaining_word: str = ""
+    remaining_fragment: str = ""
 
 
 class FindResult(NamedTuple):
@@ -50,7 +52,8 @@ class TreeNode:
 
         # if there is no matched, add the fragment here
         if not split.matched_prefix:
-            node.children[split.remaining_word] = TreeNode(word)
+            node.children[split.remaining_fragment] = TreeNode(word)
+            print(self, file=sys.stderr)
             return
 
         # if there is any remaining prefix, a split is necessary
@@ -63,35 +66,49 @@ class TreeNode:
             node = newnode
 
         # if there are any characters left, create a new child node for them
-        if split.remaining_word:
-            node.children[str(split.remaining_word)] = TreeNode(word)
+        if split.remaining_fragment:
+            node.children[str(split.remaining_fragment)] = TreeNode(word)
         else:
             node.set_is_word(word)  # if there is nothing left, it was an exact match
+        print(self, file=sys.stderr)
 
     def find_word(self, word: str):
         pass
 
     def _find_closest_match(self, fragment: str) -> FindResult:
 
-        currentNode: TreeNode = self
-        emptySplit: SplitResult = SplitResult(remaining_word=fragment)
+        result: FindResult = FindResult(node=self, split=SplitResult(remaining_fragment=fragment))
+        print(f"looking for {fragment}", file=sys.stderr)
 
-        # loop through each edge to find a match
-        for edge, node in currentNode.children.items():
-            split: SplitResult = TreeNode._split_word(fragment, edge)
+        # on exact match, return the matched node and prefix
+        if node := self.children.get(fragment, False):
+            result = FindResult(cast(TreeNode, node), SplitResult(matched_prefix=fragment))
+        else:
+            # loop through each edge to find a match
+            for edge, node in self.children.items():
+                split: SplitResult = TreeNode._split_word(fragment, edge)
+                if split.matched_prefix:
+                    print(split, file=sys.stderr)
+                    result = FindResult(self, split)
 
-            if not split.matched_prefix:
-                continue
+            # if there is no remaining prefix, keep searching
+            if result.split.matched_prefix and not result.split.remaining_prefix:
+                result = result.node.children[result.split.matched_prefix]._find_closest_match(
+                    result.split.remaining_fragment
+                )
 
-            if split.remaining_prefix:
-                return FindResult(currentNode, split)
+            # if not split.matched_prefix:
+            # continue
 
-            if split.remaining_word:
-                return node._find_closest_match(split.remaining_word)
-            else:
-                return FindResult(node, split)
+            # if split.remaining_prefix:
+            # return FindResult(currentNode, split)
 
-        return FindResult(currentNode, emptySplit)
+            # if split.remaining_fragment:
+            # return node._find_closest_match(split.remaining_fragment)
+            # else:
+            # return FindResult(node, split)
+
+        return result
         # # if we ave an exact match, return it
         # if TreeEdge(fragment) in self._children.keys():
         # return FindResult(node=self, split=SplitResult(matched_prefix=fragment))
@@ -106,10 +123,10 @@ class TreeNode:
         # if split.remaining_prefix:
         # return FindResult(node=self, split=split)
 
-        # if split.remaining_word != fragment:
-        # return node._find_closest_match(split.remaining_word)
+        # if split.remaining_fragment != fragment:
+        # return node._find_closest_match(split.remaining_fragment)
 
-        # return FindResult(node=self, split=SplitResult(remaining_word=fragment))
+        # return FindResult(node=self, split=SplitResult(remaining_fragment=fragment))
 
     @staticmethod
     def _split_word(word: str, edge: str) -> SplitResult:
@@ -120,8 +137,8 @@ class TreeNode:
         return SplitResult(
             matched_prefix=edge[:idx],
             remaining_prefix=edge[idx:],
-            remaining_word=word[idx:],
+            remaining_fragment=word[idx:],
         )
 
     def __repr__(self):
-        return ", ".join(f'{{"{e}-{n._word}": [{n}]}}' for e, n in self.children.items())
+        return ", ".join(f'{{"{e}-{n._word}-{n._count}": [{n}]}}' for e, n in self.children.items())
