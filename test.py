a6d3e54bba6cf68f86b23315b1e99e16cb946cda
Jay <jay@resolutionbio.com>
2021-08-08 00:49:21 -0700

    use pickle

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 from RadixTree import RadixTree
 from TreeNode import TreeNode
-from memory_profiler import profile
 
 import sys
 from pathlib import Path
-from json import dumps, loads
+from pickle import loads, dumps
 
 
-@profile
 def main(args):
     tree = RadixTree()
     root = TreeNode()
@@ -17,30 +15,12 @@ def main(args):
             tree.store_word(line.rstrip())
             root.store_word(line.rstrip())
 
+    treebytes = dumps(tree)
+    rootbytes = dumps(root)
+
     print(tree)
-    print(get_size(tree), file=sys.stderr)
-    print(get_size(root), file=sys.stderr)
-
-
-def get_size(obj, seen=None):
-    """Recursively finds size of objects"""
-    size = sys.getsizeof(obj)
-    if seen is None:
-        seen = set()
-    obj_id = id(obj)
-    if obj_id in seen:
-        return 0
-    # Important mark as seen *before* entering recursion to gracefully handle
-    # self-referential objects
-    seen.add(obj_id)
-    if isinstance(obj, dict):
-        size += sum([get_size(v, seen) for v in obj.values()])
-        size += sum([get_size(k, seen) for k in obj.keys()])
-    elif hasattr(obj, "__dict__"):
-        size += get_size(obj.__dict__, seen)
-    elif hasattr(obj, "__iter__") and not isinstance(obj, (str, bytes, bytearray)):
-        size += sum([get_size(i, seen) for i in obj])
-    return size
+    print(f"tree: {sys.getsizeof(treebytes)}")
+    print(f"root: {sys.getsizeof(rootbytes)}")
 
 
 if __name__ == "__main__":
